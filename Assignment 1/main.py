f8845df98d6759057ffd93cb6a5015f289e5a9ce
Levi <leviszaboo@icloud.com>
2023-09-16 11:41:33 +0200

    ex2 and 3 for real this time

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -9,7 +9,7 @@ data = pd.read_csv("Assignment 1/data_assign_p1.csv")
 quarters = data['obs']
 gdp_growth = data['GDP_QGR']
 
-# Exercise 1
+## Exercise 1
 
 # Plot GDP growth
 
@@ -49,18 +49,40 @@ ax2.grid(True)
 plt.tight_layout()
 plt.show()
 
-# Exercise 2
 
-p_max = 4
+## Exercise 2
+
+p = 4
 significance_level = 0.05
 
-# Fit the AutoReg model with general-to-specific modeling approach
-best_model = None
-best_lags = 0
+significant_lags = []
 
-for p in range(1, p_max + 1):
-    model = AutoReg(gdp_growth, lags=p, old_names=False)
+for lag in range(p, 0, -1):
+    model = AutoReg(gdp_growth, lags=lag, old_names=False)
     results = model.fit()
     
-    print(results.summary())
+    if all(results.pvalues[1:] < significance_level):
+        significant_lags.append((lag, results))
 
+for lag_order, best_model in significant_lags:
+    print(f"Best model with {lag_order} lag(s):")
+    print(best_model.summary())
+
+
+## Exercise 3
+# Note: since we only have one significant model this for loop is not necessary,
+# but it's better for reusability.
+
+acf_plots = {}
+resid_lags = 19
+
+for lag, model in significant_lags:
+    residuals = model.resid
+
+    acf_plot = plot_acf(residuals, lags=resid_lags, title=f'ACF of Residuals (AR({lag}))')
+    acf_plot.axes[0].set_xlabel('Lags')
+
+    acf_plots[lag] = acf_plot
+
+for lag, acf_plot in acf_plots.items():
+    plt.show()
